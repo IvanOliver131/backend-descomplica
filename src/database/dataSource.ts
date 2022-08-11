bf802f1fbb289d8bd6e42c095f73034224add89d
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-08-11 10:21:27 -0300

    make it better

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -1,48 +1,63 @@
 import { DataSource } from "typeorm"
 require("dotenv").config();
 
-export const PostgresDataSource = () => {
-  if (process.env.NODE_ENV === 'development') {
-    new DataSource({
-      type: "postgres",
-      host: process.env.TYPEORM_HOST,
-      port: 5432,
-      username: process.env.TYPEORM_USERNAME,
-      password: process.env.TYPEORM_PASSWORD,
-      database: process.env.TYPEORM_DATABASE,
-      synchronize: false,
-      migrations: ["src/database/migrations/*.ts"],
-      entities: ["src/models/*.ts"],
-    })
-  } else if (process.env.NODE_ENV === 'test') {
-    new DataSource({
-      type: "postgres",
-      host: process.env.TYPEORM_HOST,
-      port: 5432,
-      username: process.env.TYPEORM_USERNAME,
-      password: process.env.TYPEORM_PASSWORD,
-      database: process.env.TYPEORM_DATABASE,
-      synchronize: false,
-      migrations: ["src/database/migrations/*.ts"],
-      entities: ["src/models/*.ts"],
-    })
-  } else {
-    new DataSource({
-      type: "postgres",
-      host: process.env.TYPEORM_HOST,
-      port: 5432,
-      username: process.env.BD_USERNAME_PROD,
-      password: process.env.BD_PASSWORD_PROD,
-      database: process.env.BD_DATABASE_PROD,
-      extra: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      },
-      migrations: ['build/src/database/migrations/*.js'],
-      entities: ['build/src/models/*.js'],
-    })
-  }
+export const PostgresDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.TYPEORM_HOST,
+  port: 5432,
+  username: process.env.BD_USERNAME_PROD,
+  password: process.env.BD_PASSWORD_PROD,
+  database: process.env.BD_DATABASE_PROD,
+  extra: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  migrations: ['build/src/database/migrations/*.js'],
+  entities: ['build/src/models/*.js'],
+})
+  // if (process.env.NODE_ENV === 'development') {
+  //   new DataSource({
+  //     type: "postgres",
+  //     host: process.env.TYPEORM_HOST,
+  //     port: 5432,
+  //     username: process.env.TYPEORM_USERNAME,
+  //     password: process.env.TYPEORM_PASSWORD,
+  //     database: process.env.TYPEORM_DATABASE,
+  //     synchronize: false,
+  //     migrations: ["src/database/migrations/*.ts"],
+  //     entities: ["src/models/*.ts"],
+  //   })
+  // } else if (process.env.NODE_ENV === 'test') {
+  //   new DataSource({
+  //     type: "postgres",
+  //     host: process.env.TYPEORM_HOST,
+  //     port: 5432,
+  //     username: process.env.TYPEORM_USERNAME,
+  //     password: process.env.TYPEORM_PASSWORD,
+  //     database: process.env.TYPEORM_DATABASE,
+  //     synchronize: false,
+  //     migrations: ["src/database/migrations/*.ts"],
+  //     entities: ["src/models/*.ts"],
+  //   })
+  // } else {
+  //   new DataSource({
+  //     type: "postgres",
+  //     host: process.env.TYPEORM_HOST,
+  //     port: 5432,
+  //     username: process.env.BD_USERNAME_PROD,
+  //     password: process.env.BD_PASSWORD_PROD,
+  //     database: process.env.BD_DATABASE_PROD,
+  //     extra: {
+  //       ssl: {
+  //         require: true,
+  //         rejectUnauthorized: false,
+  //       },
+  //     },
+  //     migrations: ['build/src/database/migrations/*.js'],
+  //     entities: ['build/src/models/*.js'],
+  //   })
+  // }
 }
 
