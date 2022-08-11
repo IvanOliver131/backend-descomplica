14c89d8c09a362c519c030c942711f2c2a50ad77
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-08-11 11:08:15 -0300

    make it better

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -1,50 +1,49 @@
 import { DataSource } from "typeorm"
 require("dotenv").config();
+let PostgresDataSource: DataSource;
 
-export const PostgresDataSource = (): DataSource => {
-  let connection: DataSource;
-  if (process.env.NODE_ENV == 'development') {
-    connection = new DataSource({
-      type: "postgres",
-      host: process.env.TYPEORM_HOST,
-      port: 5432,
-      username: process.env.TYPEORM_USERNAME,
-      password: process.env.TYPEORM_PASSWORD,
-      database: process.env.TYPEORM_DATABASE,
-      synchronize: false,
-      migrations: ["src/database/migrations/*.ts"],
-      entities: ["src/models/*.ts"],
-    })
-  } else if (process.env.NODE_ENV == 'test') {
-    connection = new DataSource({
-      type: "postgres",
-      host: process.env.TYPEORM_HOST,
-      port: 5432,
-      username: process.env.TYPEORM_USERNAME,
-      password: process.env.TYPEORM_PASSWORD,
-      database: process.env.TYPEORM_DATABASE,
-      synchronize: false,
-      migrations: ["src/database/migrations/*.ts"],
-      entities: ["src/models/*.ts"],
-    })
-  } else {
-    connection = new DataSource({
-      type: "postgres",
-      host: process.env.BD_HOST_PROD,
-      port: 5432,
-      username: process.env.BD_USERNAME_PROD,
-      password: process.env.BD_PASSWORD_PROD,
-      database: process.env.BD_DATABASE_PROD,
-      extra: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
+if (process.env.NODE_ENV == 'development') {
+  PostgresDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.TYPEORM_HOST,
+    port: 5432,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+    synchronize: false,
+    migrations: ["src/database/migrations/*.ts"],
+    entities: ["src/models/*.ts"],
+  })
+} else if (process.env.NODE_ENV == 'test') {
+  PostgresDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.TYPEORM_HOST,
+    port: 5432,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+    synchronize: false,
+    migrations: ["src/database/migrations/*.ts"],
+    entities: ["src/models/*.ts"],
+  })
+} else {
+  PostgresDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.BD_HOST_PROD,
+    port: 5432,
+    username: process.env.BD_USERNAME_PROD,
+    password: process.env.BD_PASSWORD_PROD,
+    database: process.env.BD_DATABASE_PROD,
+    extra: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
       },
-      migrations: ['build/src/database/migrations/*.js'],
-      entities: ['build/src/models/*.js'],
-    })
-  }
+    },
+    migrations: ['build/src/database/migrations/*.js'],
+    entities: ['build/src/models/*.js'],
+  })
+}
 
-  return connection;
-};
+
+export { PostgresDataSource };
